3e6719e66f8a3fc49a89c65cd452daadb0011e41
ddipperq <dimonbyt@gmail.com>
2024-02-13 18:53:45 +0300

    add ticker

---
+++ src/scripts/buttonHandler.js
@@ -0,0 +1,12 @@
+function ClickFront() {
+   /*var divFront = document.querySelector(".code-front");
+   var divBack = document.querySelector(".code-back");
+ 
+   if (divFront.style.display === "none") {
+     divFront.style.display = "block";
+     divBack.style.display = "none";
+   } else {
+     divFront.style.display = "none";
+     divBack.style.display = "block";
+   }*/
+ }
